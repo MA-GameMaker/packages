7a63a9c4fd376e351e288ca6f131000e1fb001ef
AceKiron <mxace3157@outlook.com>
2025-02-05 19:12:18 +0100

    Added YYRunnerInterface source code

---
+++ yyrunnerinterface/codes/YYRunnerInterface.cpp
@@ -0,0 +1,15 @@
+#include "YYRunnerInterface.h"
+
+#include <memory.h>
+
+static YYRunnerInterface g_YYRunnerInterface{};
+YYRunnerInterface* g_pYYRunnerInterface;
+__declspec(dllexport) void YYExtensionInitialise(const struct YYRunnerInterface* _struct, size_t _size) {
+	if (_size < sizeof(YYRunnerInterface)) {
+		memcpy(&g_YYRunnerInterface, _struct, _size);
+	}
+	else {
+		memcpy(&g_YYRunnerInterface, _struct, sizeof(YYRunnerInterface));
+	}
+	g_pYYRunnerInterface = &g_YYRunnerInterface;
+}
